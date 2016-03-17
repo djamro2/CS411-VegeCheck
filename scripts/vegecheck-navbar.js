c26eccd4a901fc478bc16f707eb1645e5e8053f9
Daniel Jamrozik <djamro2@illinois.edu>
2016-03-17 16:03:29 -0500

    get navbar template from other url

--- scripts/vegecheck-navbar.js
+++ scripts/vegecheck-navbar.js
@@ -1,10 +1,10 @@
 
 'use strict';
 
-app.directive('vegecheckNavbar', function(){
+app.directive('vegecheckNavbar', function($sce){
 	return {
 	    restrict: 'AE',
-        templateUrl: 'scripts/vegecheckNavbar.html',
+        templateUrl: $sce.trustAsResourceUrl('http://web.engr.illinois.edu/~djamro2/vegecheckNavbar.html'),
 		replace: 'true',
         link: function(scope, element, attrs) {
 
