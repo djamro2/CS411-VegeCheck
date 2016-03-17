60e0e3f77c581fd5b4b5f3b757248dea9ddda636
Daniel Jamrozik <djamro2@illinois.edu>
2016-03-17 15:22:16 -0500

    create delete_quiz.php

---
+++ delete_quiz.php
@@ -0,0 +1,22 @@
+
+<?php
+
+// used to login to the database
+require("util/database.php");
+
+// attributes for this item
+$quizId = $_POST["quizId"];
+
+//Insert the image name and image content in image_table
+$delete_quiz = sprintf("DELETE FROM quiz WHERE quizID = %d", $quizId);
+
+$query_result = mysql_query($delete_quiz);
+
+if ($query_result == false) {
+    echo "Delete quiz failed";
+} else {
+    header("Location: quiz_overview_all.php");
+    exit();
+}
+
+?>
