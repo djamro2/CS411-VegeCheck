0ff24d782471b40a86636263dd8e56748c079f0d
Daniel Jamrozik <djamro2@illinois.edu>
2016-03-17 16:22:18 -0500

    make quiz title editable

--- quiz_overview_code.php
+++ quiz_overview_code.php
@@ -7,7 +7,7 @@
 	if(mysql_num_rows($result) > 0) {
 		while($row = mysql_fetch_assoc($result)) {
 			echo "<md-list-item layout=\"row\" layout-align=\"none center\">" . 
-                        "<span>" . $row["name"] . "</span>" .
+                        "<a class='title-link' href='taking_quiz.php'><span class='quiz-title'>" . $row["name"] . "</span></a>" .
                         "<span flex></span>" . 
                         "<i class=\"material-icons settings-icon\" ng-click=\"goTo('create_quiz.php?quizId=" . $row["quizID"] . "')\">build</i>" .
                         "<form method=\"POST\" action=\"delete_quiz.php\">" .
